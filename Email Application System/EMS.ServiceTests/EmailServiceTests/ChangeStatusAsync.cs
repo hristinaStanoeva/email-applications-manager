2c542e04b8b8e337bafa26d6f1d2ce6e1a09661e
SashoTatarski <sasho56@hotmail.com>
2019-11-22 17:59:09 +0200

    EmailSerive Tests Complete

--- Email Application System/EMS.ServiceTests/EmailServiceTests/ChangeStatusAsync.cs
+++ Email Application System/EMS.ServiceTests/EmailServiceTests/ChangeStatusAsync.cs
@@ -36,5 +36,54 @@ namespace EMS.ServiceTests.EmailServiceTests
                 Assert.IsTrue(email.Status == EmailStatus.Open);
             }
         }
+
+
+        [TestMethod]
+        public async Task ChangeStatusToNew()
+        {
+            TestUtils.GetContextWithEmails(nameof(ChangeStatusToNew));
+
+            var gmailServiceMock = new Mock<IGmailAPIService>();
+
+            using (var assertContext = new SystemDataContext(TestUtils.GetOptions(nameof(ChangeStatusToNew))))
+            {
+                var sut = new EmailService(assertContext, gmailServiceMock.Object);
+
+                var id = TestUtils.Emails[0].Id;
+
+                await sut.ChangeStatusAsync(id.ToString(), EmailStatus.New);
+
+                var email = assertContext.Emails.FirstOrDefault(mail => mail.Id == id);
+
+                var date = new DateTime(2019, 11, 22);
+                email.ToNewStatus = date;
+
+                Assert.IsTrue(email.ToNewStatus == date);
+            }
+        }
+
+        [TestMethod]
+        public async Task ChangeStatusToClosed()
+        {
+            TestUtils.GetContextWithEmails(nameof(ChangeStatusToClosed));
+
+            var gmailServiceMock = new Mock<IGmailAPIService>();
+
+            using (var assertContext = new SystemDataContext(TestUtils.GetOptions(nameof(ChangeStatusToClosed))))
+            {
+                var sut = new EmailService(assertContext, gmailServiceMock.Object);
+
+                var id = TestUtils.Emails[0].Id;
+
+                await sut.ChangeStatusAsync(id.ToString(), EmailStatus.Closed);
+
+                var email = assertContext.Emails.FirstOrDefault(mail => mail.Id == id);
+
+                var date = new DateTime(2019, 11, 22);
+                email.ToTerminalStatus = date;
+
+                Assert.IsTrue(email.ToTerminalStatus == date);
+            }
+        }
     }
 }
