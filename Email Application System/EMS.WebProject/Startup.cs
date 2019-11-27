105fd19c6fe5339ebafc0b27d29b8965d22511a5
SashoTatarski <sasho56@hotmail.com>
2019-11-27 12:27:03 +0200

    ErrorPage Implemented

--- Email Application System/EMS.WebProject/Startup.cs
+++ Email Application System/EMS.WebProject/Startup.cs
@@ -75,6 +75,7 @@ namespace EMS.WebProject
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
